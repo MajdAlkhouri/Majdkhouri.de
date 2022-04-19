c5cddc0f6847d455cba3015aec227e00c17fadd6
MajdAlkhouri <mjodizaodiz@gmail.com>
2022-04-19 17:34:28 +0200

    Particles

---
+++ src/app/contact/contact.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'app-contact',
+  templateUrl: './contact.component.html',
+  styleUrls: ['./contact.component.scss']
+})
+export class ContactComponent implements OnInit {
+  contact = {
+    name: '', 
+    email: '', 
+    message: '', 
+  };
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
